c92a64edc31c617872e2003314c9b66091b36849
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-04-08 06:05:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'MrKillerDeveloper')
 API_ID = int(environ.get('API_ID', '1508388'))
 API_HASH = environ.get('API_HASH', '9b83ad0f6f0eeb973c8640258ea515c7')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6122039491:AAEjUS6zyKVvhF7eIroF8kqDAeKUS3cbTqc")
 
 #Port
 PORT = environ.get("PORT", "8080")
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Harshil1109:harshil1109@cluster0.mrsoi.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
